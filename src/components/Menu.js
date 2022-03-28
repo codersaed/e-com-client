deb251a7118221ad549b3ac1e89f7b9d5baeaaa4
codersaed <codersaed@gmail.com>
2022-03-28 17:03:38 +0600

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { singout, isAuthenticated, userInfo } from '../utils/auth';
 
-const Menu = () => {
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: '#ff9900' }
+    } else {
+        return { color: 'grey' }
+    }
+}
+
+const Menu = ({ history }) => {
     return (
         <nav className='navbar navbar-dark bg-dark'>
             <ul className="nav nav-tabs" >
                 <li className="nav-item">
-                    <Link className="nav-link">Home</Link>
+                    <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link">Register</Link>
-                </li>
+                {!isAuthenticated() && (<>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/login')} to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/register')} to="/register">Register</Link>
+                    </li>
+                </>)}
+
+                {isAuthenticated() && (<>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, `/${userInfo().role}/dashboard`)} to={`/${userInfo().role}/dashboard`}>Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, `/cart`)} to={`/cart`}>Cart</Link>
+                    </li>
+                    <li className="nav-item">
+                        <span className="nav-link" style={{ cursor: 'pointer', color: 'grey' }} onClick={() => {
+                            singout(() => {
+                                history.push('/login')
+                            });
+                        }}> Log Out</span>
+                    </li>
+                </>)}
             </ul>
         </nav>
     )
 }
 
-export default Menu;
+export default withRouter(Menu);
